a600739ce2046f2644ac6f9cc20250220cc52617
anthonyvigz <anthonyvigz@gmail.com>
2019-08-20 20:38:58 -0400

    setting up router get with knex

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,7 +6,15 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    // SELECT * FROM Posts
+    db('posts')
+    .then((posts) => {
+        res.json(posts)
+    })
+    .catch((error) => {
+        res.status(500).json({ message: 'Failed to get posts.' })
+    })
+    
 });
 
 router.get('/:id', (req, res) => {
