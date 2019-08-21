ef33e91d43545528a1befff429a63567c001b4e7
anthonyvigz <anthonyvigz@gmail.com>
2019-08-20 20:50:35 -0400

    set up post finder via id

--- posts/post-router.js
+++ posts/post-router.js
@@ -18,7 +18,15 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    const { id } = req.params;
 
+    db('posts').where('id', id)
+    .then((post) => {
+        res.json(post)
+    })
+    .catch((error) => {
+        res.status(500).json({ message: 'Failed to get post.' })
+    })
 });
 
 router.post('/', (req, res) => {
